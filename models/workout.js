2f2d4380fdf9e1e841231cd62874d8615f3e5a08
dp <davepollan@gmail.com>
2020-03-25 22:48:00 -0400

    core files added

---
+++ models/workout.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    type: { type: String, required: true },
+    name: { type: String, required: true },
+    duration: Number,
+    weight: Number,
+    reps: Number,
+    sets: Number,
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
