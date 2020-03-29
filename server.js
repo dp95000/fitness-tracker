132ca816019ab78ab6e2c7090d5938c63af9e26b
dp <davepollan@gmail.com>
2020-03-28 20:28:06 -0400

    heroku DB code added

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const compression = require("compression");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// if deployed, use the deployed database. Otherwise use the local mogoHealines database.
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workouttracker";
+
 app.use(compression());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -13,9 +16,11 @@ app.use(express.json());
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouttracker", {
-    useNewUrlParser: true
-});
+mongoose.connect(MONGODB_URI);
+
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouttracker", {
+//    useNewUrlParser: true
+//});
 
 app.listen(PORT, function() {
     console.log(`Now listening on port: ${PORT}`);
